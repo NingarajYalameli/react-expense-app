988fff619f4ce4edbef038d68f9548045813c3a2
ningaraj <nb123@gmail.com>
2023-03-16 10:39:13 +0530

    second commit

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -10,7 +10,7 @@ const ExpensesFilter = (props) => {
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
-        <label>Filter by year</label>
+        <label>Filter based on year</label>
         <select value={props.selected} onChange={dropdownChangeHandler}>
           <option value="2025">2025</option>
           <option value="2024">2024</option>
